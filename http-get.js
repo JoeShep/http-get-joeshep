8392d6f537685386d233f9ba8b5afe45ec6576ef
Joe Shepherd <joeshepmedia@gmail.com>
2017-03-22 10:08:43 -0500

    Add error handler

--- http-get.js
+++ http-get.js
@@ -3,6 +3,22 @@ const { readFile } = require('fs');
 
 // AJAX-like request
 get('https://node-http.firebaseio.com/words.json', (res) => {
+  const statusCode = res.statusCode;
+  const contentType = res.headers['content-type'];
+
+  let error = null;
+  if (statusCode !== 200) {
+    error = new Error(`Request Failed.\n Status Code: ${statusCode}`);
+  } else if (!/^application\/json/.test(contentType)) {
+    error = new Error(`Invalid content-type.\n Expected application/json but received ${contentType}`);
+  }
+
+  if (error) {
+    console.log(error.message);
+    res.resume();
+    return;
+  }
+
   let body = '';
   res.on('data', (buff) => {
     console.log("status?", res.statusCode);
